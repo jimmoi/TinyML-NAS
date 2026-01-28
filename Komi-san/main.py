d479bdd5b283bb01b08aec09e287c0f7e377396a
Nah man, i lose. <selaruang@gmail.com>
2026-01-28 21:10:15 +0700

    add util function

--- Komi-san/main.py
+++ Komi-san/main.py
@@ -1,53 +1,14 @@
 from tensorflow import keras
 from pathlib import Path
 import tensorflow as tf
-import numpy as np
 from experiment_manager import Manager
 from Optimizer import *
+from my_util import load_dataset, test_tflite_model
 import sys
 import json
 import pickle
 import matplotlib.pyplot as plt
-
-
-# search_space = ArchitectureSearchSpace(k_range=(2, 10), c_range=(1, 5))
-# decoder = ModelDecoder()
-
-# # Pass your existing evaluation logic here
-# colabNAS = ColabNAS(peak_RAM_upper_bound, Flash_upper_bound, MACC_upper_bound, path_to_training_set, val_split, cache, input_shape, save_path=save_path)
-# #search
-# path_to_tflite_model = colabNAS.search(NASPsoOptimizer.setup(search_space, decoder))
-
-def test_tflite_model(path_to_resulting_architecture, test_ds):
-    # Convert the Path object to a string
-    interpreter = tf.lite.Interpreter(model_path=str(path_to_resulting_architecture))
-    interpreter.allocate_tensors()
-
-    output = interpreter.get_output_details()[0]  # Model has single output.
-    input = interpreter.get_input_details()[0]  # Model has single input.
-    input_dtype = input['dtype']
-
-    correct = 0
-    wrong = 0
-
-    for images_batch, labels_batch in test_ds:
-        for image, label in zip(images_batch, labels_batch):
-            # Check if the input type is quantized, then rescale input data to uint8
-            if input_dtype == np.uint8 or input_dtype == tf.uint8:
-                input_scale, input_zero_point = input["quantization"]
-                image = image / input_scale + input_zero_point
-            input_data = np.expand_dims(image.numpy().astype(input_dtype), axis=0)
-            interpreter.set_tensor(input['index'], input_data)
-            interpreter.invoke()
-            if label.numpy().argmax() == interpreter.get_tensor(output['index']).argmax() :
-                correct = correct + 1
-            else :
-                wrong = wrong + 1
-    print(f"\nTflite model test accuracy: {correct/(correct+wrong)}")
     
-    return correct/(correct+wrong)    
-    
-
 def main():
     
     import os
@@ -73,7 +34,7 @@ def main():
     
     _, test_ds = nas.get_data()
     tflite_accuracy = test_tflite_model(search_output['path_to_best_architecture'], test_ds)
-    search_output["tflite_accuracy"] = np.round(tflite_accuracy, 4)
+    search_output["tflite_accuracy"] = round(tflite_accuracy, 4)
     
     manager.visualize(search_output)
     
